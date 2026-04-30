c08eb9f7946cf79876ec096125676c803ac998bc
Gustav Olaf Yunus Laitinen-Fredriksson Lundström Imanov <olaf.laitinen@gmail.com>
2026-04-30 17:40:30 +0200

    Update build configuration and environment API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: typeof window !== 'undefined' && (window as any).API_URL
     ? (window as any).API_URL
-    : 'https://o9bsy5k3wl.execute-api.eu-north-1.amazonaws.com'
+    : 'https://9x38j33l1h.execute-api.eu-north-1.amazonaws.com'
 };
